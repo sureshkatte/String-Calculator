667800df3623d6b1f6010f37a977eb355bcdb2a7
sureshkatte <sureshbabuitsoft@gmail.com>
2025-01-04 16:27:50 +0530

    Written one test case as it is failing because no service file

---
+++ spec/services/calculator_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Calculator, type: service do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(Calculator.add("")).to eq(0)
+    end
+  end
+end
