9b1288841a328e64d894e9725b34259bcbbb25f8
sureshkatte <sureshbabuitsoft@gmail.com>
2025-01-04 17:02:38 +0530

    Created controller and written routes and one positive test case

---
+++ spec/requests/calculators_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Calculators", type: :request do
+  describe "GET /calculate" do
+    it "returns the correct result for valid input" do
+      get '/calculators/add', params: { input: 5 }
+      expect(response).to have_http_status(:success)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['result']).to eq(5)
+    end
+  end
+end
