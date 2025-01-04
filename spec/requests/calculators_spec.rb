bba360d5b80aece038ac08905d28aa6d0562145a
sureshkatte <sureshbabuitsoft@gmail.com>
2025-01-04 17:07:59 +0530

    added testcase for input empty also

--- spec/requests/calculators_spec.rb
+++ spec/requests/calculators_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Calculators", type: :request do
   describe "GET /calculate" do
-    it "returns the correct result for valid input" do
+    it "returns the correct result for input given" do
       get '/calculate', params: { input: 5 }
       expect(response).to have_http_status(:success)
 
       json_response = JSON.parse(response.body)
       expect(json_response['result']).to eq(5)
+    end
+
+    it "returns the zero as  for input is blank" do
+      get '/calculate', params: { input: '' }
+      expect(response).to have_http_status(:success)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['result']).to eq(0)
     end
   end
 end
