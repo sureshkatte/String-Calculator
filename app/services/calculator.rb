9bc0844e1611f20540d95063d7db427175591868
sureshkatte <sureshbabuitsoft@gmail.com>
2025-01-04 16:44:46 +0530

    Adding source code for supporting multiple delimeters

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,10 +2,21 @@ class Calculator
   def self.add(input)
     return 0 if input.blank?
 
-    delimiter = ','
+    if input.start_with?("//")
+      delimiter, input = parse_custom_delimeter(input)
+    else
+      delimiter = /,|\n/
+    end
 
     numbers = input.split(delimiter).map(&:to_i)
 
     numbers.sum
+  end
+
+  private
+
+  def self.parse_custom_delimeter(input)
+    match = input.match(%r{//(.)\n(.*)})
+    [Regexp.escape(match[1]), match[2]]
   end
 end
